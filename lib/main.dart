26091ccddf55cd592711b025e3da3fbc18791721
DiegoPorta20 <u202019287@upc.edu.pe>
2024-06-13 16:09:58 -0500

    feat: Insert notification service

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,27 @@
 import 'package:agrotech_mobile/notification/notification_service.dart';
 import 'package:agrotech_mobile/pages/IdentityAndAccessManagement/view/SignIn.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  LocalNotificationService().requestPermition();
+  
+  await LocalNotificationService().requestPermition();
+  await LocalNotificationService().init();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+ 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -38,8 +43,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  void initState() {
+    notificationHandler();
+    super.initState();
+  }
+
+  void notificationHandler(){
+    FirebaseMessaging.onMessage.listen((event) async{
+      LocalNotificationService().showNotification(event);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    LocalNotificationService().uploadFcmToken();
     return Scaffold(
         body: Container(
       child: SignIn(),
--- lib/pages/IdentityAndAccessManagement/view/SignIn.dart
+++ lib/pages/IdentityAndAccessManagement/view/SignIn.dart
@@ -1,9 +1,11 @@
+import 'package:agrotech_mobile/notification/notificationService.dart';
 import 'package:agrotech_mobile/pages/IdentityAndAccessManagement/model/account.dart';
 import 'package:agrotech_mobile/pages/IdentityAndAccessManagement/model/farmer.dart';
 import 'package:agrotech_mobile/pages/IdentityAndAccessManagement/model/signIn.dart';
 import 'package:agrotech_mobile/pages/IdentityAndAccessManagement/services/FarmerService.dart';
 import 'package:agrotech_mobile/pages/IdentityAndAccessManagement/services/SignInService.dart';
 import 'package:agrotech_mobile/pages/IrrigationManagement/view/principalView.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -19,6 +21,7 @@ class _SignInState extends State<SignIn> {
   SignInModel? signInModel;
   Account? account;
   Farmer? farmer;
+  List<DocumentSnapshot>? documents;
   HttpSignInService? signInService;
   FarmerService? farmerService;
   TextEditingController emailAddress = TextEditingController();
@@ -65,117 +68,139 @@ class _SignInState extends State<SignIn> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
-        body: Stack(
-          fit: StackFit.expand,
-          children: [
-            Positioned(
-              left: 0,
-              right: 0,
-              bottom: 0,
-              child: Image.asset(
-                'assets/Vector.png', // Ruta de la imagen de fondo
-                fit: BoxFit.cover,
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(
-                left: 25,
-                right: 25,
-              ),
-              child: Center(
-                  child: ListView(children: [
-                Container(
-                  margin: const EdgeInsets.only(top: 40, bottom: 15),
-                  height: 200,
-                  child: Image.asset('assets/logo.png'),
-                ),
-                Container(
-                  margin: EdgeInsets.only(bottom: 15, top: 15),
-                  child: Text(
-                    'Iniciar Sesión',
-                    style: GoogleFonts.poppins(
-                      fontWeight: FontWeight.w700,
-                      fontSize: 22, // Tamaño de fuente más grande
-                    ),
+        body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection('users').snapshots(),
+          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshots){
+            if(snapshots.connectionState == ConnectionState.waiting){
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if(snapshots.hasError){
+              return Center(
+                child: Text('Error: ${snapshots.error}'),
+              );
+            }
+            documents = snapshots.data!.docs;
+            return Stack(
+              fit: StackFit.expand,
+              children: [
+                Positioned(
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  child: Image.asset(
+                    'assets/Vector.png', // Ruta de la imagen de fondo
+                    fit: BoxFit.cover,
                   ),
                 ),
                 Container(
-                  margin: EdgeInsets.only(bottom: 15),
-                  child: TextField(
-                    controller: emailAddress,
-                    decoration: InputDecoration(
-                      labelText: 'Correo Electrónico',
-                      labelStyle: TextStyle(
-                        fontFamily: 'Outfit',
-                        fontWeight: FontWeight.w500,
-                        fontSize: 14,
-                        color: Color(0xFF635C5C),
-                      ),
-                      border: OutlineInputBorder(),
-                      // Otros atributos de decoración que desees agregar
-                    ),
+                  margin: const EdgeInsets.only(
+                    left: 25,
+                    right: 25,
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(bottom: 15),
-                  child: TextField(
-                    controller: password,
-                    decoration: InputDecoration(
-                      labelText: 'Contraseña',
-                      labelStyle: TextStyle(
-                        fontFamily: 'Outfit',
-                        fontWeight: FontWeight.w500,
-                        fontSize: 14,
-                        color: Color(0xFF635C5C),
-                      ),
-                      border: OutlineInputBorder(),
-                      // Otros atributos de decoración que desees agregar
+                  child: Center(
+                      child: ListView(children: [
+                    Container(
+                      margin: const EdgeInsets.only(top: 40, bottom: 15),
+                      height: 200,
+                      child: Image.asset('assets/logo.png'),
                     ),
-                  ),
-                ),
-                Container(
-                  alignment: Alignment.bottomRight,
-                  margin: EdgeInsets.only(
-                      bottom: 15), // Posiciona el texto al final
-                  child: Text(
-                    '¿Olvidaste tu contraseña?',
-                    style: GoogleFonts.poppins(
-                      fontWeight: FontWeight.w400,
-                      fontSize: 13,
-                      color: Color(0xFF635C5C),
-                    ),
-                  ),
-                ),
-                Container(
-                    child: TextButton(
-                  onPressed: () {
-                    signInServiceAccount();
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                      Color(0xFF297739),
-                    ),
-                    minimumSize: MaterialStateProperty.all<Size>(
-                      Size(double.infinity, 60),
-                    ),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius:
-                            BorderRadius.circular(8), // Bordes redondeados
+                    Container(
+                      margin: EdgeInsets.only(bottom: 15, top: 15),
+                      child: Text(
+                        'Iniciar Sesión',
+                        style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 22, // Tamaño de fuente más grande
+                        ),
                       ),
                     ),
-                  ),
-                  child: Text(
-                    'Ingresar',
-                    style: GoogleFonts.poppins(
-                      fontSize: 20,
-                      color: Colors.white,
+                    Container(
+                      margin: EdgeInsets.only(bottom: 15),
+                      child: TextField(
+                        controller: emailAddress,
+                        decoration: InputDecoration(
+                          labelText: 'Correo Electrónico',
+                          labelStyle: TextStyle(
+                            fontFamily: 'Outfit',
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14,
+                            color: Color(0xFF635C5C),
+                          ),
+                          border: OutlineInputBorder(),
+                          // Otros atributos de decoración que desees agregar
+                        ),
+                      ),
                     ),
-                  ),
-                ))
-              ])),
-            )
-          ],
-        ));
+                    Container(
+                      margin: EdgeInsets.only(bottom: 15),
+                      child: TextField(
+                        controller: password,
+                        decoration: InputDecoration(
+                          labelText: 'Contraseña',
+                          labelStyle: TextStyle(
+                            fontFamily: 'Outfit',
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14,
+                            color: Color(0xFF635C5C),
+                          ),
+                          border: OutlineInputBorder(),
+                          // Otros atributos de decoración que desees agregar
+                        ),
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.bottomRight,
+                      margin: EdgeInsets.only(
+                          bottom: 15), // Posiciona el texto al final
+                      child: Text(
+                        '¿Olvidaste tu contraseña?',
+                        style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 13,
+                          color: Color(0xFF635C5C),
+                        ),
+                      ),
+                    ),
+                    Container(
+                        child: TextButton(
+                      onPressed: () {
+                        NotificacionService().pusherNotificaation(
+                          title: 'Hola',
+                          body: 'Mundo',
+                          token: documents![0]['notificationToken'].toString(),
+                        );
+                        //signInServiceAccount();
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                          Color(0xFF297739),
+                        ),
+                        minimumSize: MaterialStateProperty.all<Size>(
+                          Size(double.infinity, 60),
+                        ),
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.circular(8), // Bordes redondeados
+                          ),
+                        ),
+                      ),
+                      child: Text(
+                        'Ingresar',
+                        style: GoogleFonts.poppins(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ))
+                  ])),
+                )
+              ],
+            );
+          },
+        )
+        );
   }
 }
